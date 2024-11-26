0a4d3e21176758446b82fb2ea8fa2db7a5e872e9
Fredrik Pettersson <fredrik.pettersson.89@gmail.com>
2024-11-26 22:12:02 +0100

    Block ratings if competition is not started

--- components/molecules/CompetitionCard.tsx
+++ components/molecules/CompetitionCard.tsx
@@ -1,9 +1,12 @@
+import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
+import { useMemo } from 'react';
 import { Image, TouchableOpacity, View, ViewProps } from 'react-native';
+import { t, translations } from '../../i18n';
 import { toImagekitUrl } from '../../imagekit';
 import { CompetitionResponse } from '../../protos/competition';
 import { useCompetitionStore } from '../../store';
-import { FormatDate } from '../../utils/dayjs';
+import { FormatDate, isAfterNow } from '../../utils/dayjs';
 import { Card } from '../atoms/Card';
 import { Text } from '../atoms/Text';
 
@@ -22,9 +25,24 @@ export const CompetitionCard = ({ competition, ...props }: Props) => {
     router.navigate(`/competitions/${competition.id}`);
   };
 
+  const isCompetitionStarted = useMemo(
+    () => !isAfterNow(competition.start_time),
+    [competition],
+  );
+
   return (
-    <TouchableOpacity onPress={handlePress}>
+    <TouchableOpacity onPress={handlePress} disabled={!isCompetitionStarted}>
       <Card {...props}>
+        {!isCompetitionStarted && (
+          <View className="absolute w-full h-full rounded-md flex items-center justify-center bg-black/50 z-10">
+            <View className="flex flex-row items-center">
+              <Ionicons name="time" size={24} color="white" />
+              <Text className="text-white">
+                {t(translations.competition.competitionNotYetStartedText)}
+              </Text>
+            </View>
+          </View>
+        )}
         <View className="flex flex-row">
           {competition.image_url && (
             <Image
